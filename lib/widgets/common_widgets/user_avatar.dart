0899c262e3dc3198048c6a201cfb793760d38680
khaled550 <kha5775@gmail.com>
2022-08-01 01:30:38 +0400

    Update

--- lib/widgets/common_widgets/user_avatar.dart
+++ lib/widgets/common_widgets/user_avatar.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class UserAvatar extends StatelessWidget {
-  const UserAvatar({Key? key}) : super(key: key);
+  double? radius;
+  UserAvatar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const DecoratedBox(
       decoration: BoxDecoration(shape: BoxShape.circle),
       child: CircleAvatar(
-        backgroundImage:
-            AssetImage('assets/images/user_avatar_placeholder.png'),
+        radius: 50,
+        backgroundImage: AssetImage('assets/images/user_avatar_placeholder.png'),
       ),
     );
   }
